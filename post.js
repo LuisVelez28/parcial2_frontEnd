e55e1107644d2db787e3fb3b56f4bd9e6de9b4de
Messi Chiquito <luvelezg@unal.edu.co>
2023-05-08 13:06:47 -0500

    Entrega Final

--- post.js
+++ post.js
@@ -3,8 +3,7 @@ function guardar(){
     form.addEventListener('submit', function(e){
         e.preventDefault();
         const sales = new FormData(form);
-        const endPoint = 'http://127.0.0.1:8000/api/sales';
-        fetch(endPoint, {
+        fetch('http://127.0.0.1:8000/api/sales', {
             method: 'POST',
             body: sales
         })
