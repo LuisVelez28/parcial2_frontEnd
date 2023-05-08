719dc61b9cfd568807aee5aa42a82c8fd5c0e5d4
Messi Chiquito <luvelezg@unal.edu.co>
2023-05-08 12:57:44 -0500

    Se realizaron todos los ejercicios

--- post.js
+++ post.js
@@ -1,16 +1,16 @@
 function guardar(){
-    const form = document.getElementById('http://127.0.0.1:8000/api/sales');
+    const form = document.getElementById('formulario');
     form.addEventListener('submit', function(e){
         e.preventDefault();
-        const sale = new FormData(form);
-        fetch('', {
-            method: '',
-            body: sale
+        const sales = new FormData(form);
+        const endPoint = 'http://127.0.0.1:8000/api/sales';
+        fetch(endPoint, {
+            method: 'POST',
+            body: sales
         })
         .then(response => response.json())
         .then(data => {
             console.log(data);
         });
     });
-    location.reload();
 }
